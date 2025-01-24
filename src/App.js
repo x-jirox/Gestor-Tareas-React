521c42e358312bcaa9e8e2c9919a189d64ee7aec
x-jirox <correalenin045gmail.com>
2025-01-24 00:11:15 -0500

    ejercicios gestor tareas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Title from './Componenets/Fragments/Title';
-
+import ListaTareas from './Componenets/ListaTareas'
 
 function App() {
   return (
     <div className="App">
 
       <Title />
+
+      <div className='lista-tareas'>
+        <h1>Mis tareas</h1>
+        <ListaTareas />
+      </div>
 
     </div>
   );
